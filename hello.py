b99e629f0492a07b48bea20bc7e67472172e87be
Nikita Kachaev <tttonyalpha@gmail.com>
2021-06-09 16:35:53 +0300

    Modified hello.py

--- hello.py
+++ hello.py
@@ -2,13 +2,15 @@ from urllib.parse import parse_qs
 
 
 def wsgi_app(environ, start_response):
-    params = parse_qs(environ['QUERY_STRING'])
-    #params = environ['QUERY_STRING']
+    #params = parse_qs(environ['QUERY_STRING'])
+    params = environ['QUERY_STRING']
     start_response("200 OK", [("Content-Type", "text/plain"),
                               ("Content-Length", str(len(params)))])
-    #return params
-    ans = []
-    for el in params.items():
-    	ans.append(str(el[0]) + "=" + str(el[1]))
-    	ans_str = "\n".join(ans)
-    return [bytes(ans_str, 'utf-8')]
+   # return [(params.keys()[0]).encode('utf-8')]
+    body = [bytes(i + '\n', 'ascii')
+            for i in environ['QUERY_STRING'].split('&')]
+    #ans = []
+    # for el in params.items():
+    #    ans_str = str(el[0]) + "=" + str(el[1][0]) + "\n"
+    #    ans.append(bytes(ans_str, 'ascii'))
+    return body
