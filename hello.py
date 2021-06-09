dff41f1376523dbe1800c5abf1893f9a12e20138
Nikita Kachaev <tttonyalpha@gmail.com>
2021-06-09 16:03:22 +0300

    edited hello.py

--- hello.py
+++ hello.py
@@ -6,8 +6,9 @@ def wsgi_app(environ, start_response):
     #params = environ['QUERY_STRING']
     start_response("200 OK", [("Content-Type", "text/plain"),
                               ("Content-Length", str(len(params)))])
+    #return params
     ans = []
     for el in params.items():
-        ans.append(f"{el[0]}={el[1]}")
-    ans_str = "\n".join(ans)
-    return ans_str
+    	ans.append(str(el[0]) + "=" + str(el[1]))
+    	ans_str = "\n".join(ans)
+    return [bytes(ans_str, 'utf-8')]
