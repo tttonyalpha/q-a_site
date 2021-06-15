bf5bd3fdd5dde4a77a451c8352e5200682db063d
tttonyalpha <tttonyalpha@gmail.com>
2021-06-15 23:03:03 +0300

    modified 1) web/ask/qa/views.py 2) nginx.conf

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+    return HttpResponse('OK', status=200)
 # Create your views here.
