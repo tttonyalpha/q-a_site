d2a6d20eef9bb53188116d8b3c275d9994f96d69
tttonyalpha <tttonyalpha@gmail.com>
2021-06-16 22:42:58 +0300

    settings.py: add qa.apps.QaConfig in applications

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa.apps.QaConfig'
 ]
 
 MIDDLEWARE = [
