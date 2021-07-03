858b99f6eedf4f78faf03cf71a4c932f00b2f3cf
Nikita Kachaev <tttonyalpha@gmail.com>
2021-07-03 18:21:34 +0300

    edit models.py: add question manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -10,6 +10,7 @@ class Question(models.Model):
     rating = models.IntegerField(default=0)
     author = models.CharField(max_length=100)
     likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
 
 
 class Answer(models.Model):
@@ -20,8 +21,8 @@ class Answer(models.Model):
 
 
 class QuestionManager(models.Manager):
-    def new():
-        pass
+    def new(self):
+        return self.order_by('-added_at')
 
-    def popular():
-        pass
+    def popular(self):
+        return self.order_by('-rating')
